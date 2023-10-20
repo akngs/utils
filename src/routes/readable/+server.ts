e3afe35e2120a846662ade2d69520f00e7cac764
akngs <jania902@gmail.com>
2023-10-21 08:33:09 +0900

    docs: make jsdoc description shorter

--- src/routes/readable/+server.ts
+++ src/routes/readable/+server.ts
@@ -3,12 +3,7 @@ import { error } from '@sveltejs/kit'
 import { makeReadable } from '$lib/readability.js'
 
 /**
- * Fetches a URL, parses the HTML response into a readable format, and returns a new response with the parsed HTML in JSON format.
- * @param {Object} param - The parameter object.
- * @param {URLSearchParams} param.url - The URLSearchParams object with a 'url' search parameter.
- * @param {Function} param.fetch - The fetch function.
- * @returns {Promise<Response>} A Promise that resolves to a Response object with the parsed HTML in JSON format.
- * @throws {Error} Throws an error if the URL parameter is missing or if the fetch request fails.
+ * Returns the readability enhanced version of the given URL.
  */
 export const GET = async ({ url, fetch }) => {
   const u = url.searchParams.get('url')
