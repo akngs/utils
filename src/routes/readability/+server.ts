4629fcbb95c5d15f8c633699aa53131974face71
akngs <jania902@gmail.com>
2023-10-21 08:09:39 +0900

    refactor: use querystring instead of path

--- src/routes/readability/+server.ts
+++ src/routes/readability/+server.ts
@@ -1,7 +1,12 @@
+import { error } from '@sveltejs/kit'
+
 import { makeReadable } from '$lib/readability.js'
 
-export const GET = async ({ params, fetch }) => {
-  const res = await fetch(params.url)
+export const GET = async ({ url, fetch }) => {
+  const u = url.searchParams.get('url')
+  if (!u) throw error(400)
+
+  const res = await fetch(u)
   const html = await res.text()
   const parsed = makeReadable(html)
   return new Response(JSON.stringify(parsed), { headers: { 'content-type': 'application/json' } })
