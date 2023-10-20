af0498e83b34a74e941b630198b21466b4638b0c
akngs <jania902@gmail.com>
2023-10-21 08:26:30 +0900

    fix: handle error cases

--- src/routes/readable/+server.ts
+++ src/routes/readable/+server.ts
@@ -4,9 +4,11 @@ import { makeReadable } from '$lib/readability.js'
 
 export const GET = async ({ url, fetch }) => {
   const u = url.searchParams.get('url')
-  if (!u) throw error(400)
+  if (!u) throw error(400, 'url parameter is missing')
 
   const res = await fetch(u)
+  if (!res.ok) throw error(400, `Failed to fetch url. Status code: ${res.status}`)
+
   const html = await res.text()
   const parsed = makeReadable(html)
   return new Response(JSON.stringify(parsed), { headers: { 'content-type': 'application/json' } })
