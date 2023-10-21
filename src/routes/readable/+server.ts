9ac2f9d615871a662d68f3a270771346af3b9b1b
Alan Kang <jania902@gmail.com>
2023-10-22 00:05:51 +0900

    feat: add summarize API (#10)
    - Add summarization API. This feature uses the OpenAI API to generate a
    concise summary of the provided content.
    - Implemented an authorization check for requests.

--- src/routes/readable/+server.ts
+++ src/routes/readable/+server.ts
@@ -1,11 +1,15 @@
 import { error, json } from '@sveltejs/kit'
 
+import { SECRET } from '$env/static/private'
 import { makeReadable } from '$lib/readability.js'
 
 /**
  * Returns the readability enhanced version of the given URL.
  */
-export const GET = async ({ url, fetch }) => {
+export const GET = async ({ url, fetch, request }) => {
+  const authr = request.headers.get('authorization')
+  if (authr !== SECRET) throw error(401, 'Unauthorized')
+
   const u = url.searchParams.get('url')
   if (!u) throw error(400, 'url parameter is missing')
 
