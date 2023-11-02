56dc6e716908a0981df52da3404cd5f6ecde0897
akngs <jania902@gmail.com>
2023-11-02 22:29:25 +0900

    feat: remove authr

--- src/routes/readable/+server.ts
+++ src/routes/readable/+server.ts
@@ -1,19 +1,15 @@
 import { error, json } from '@sveltejs/kit'
 
-import { SECRET } from '$env/static/private'
 import { makeReadable } from '$lib/readability.js'
 
 /**
  * Returns the readability enhanced version of the given URL.
  */
-export const GET = async ({ url, fetch, request }) => {
-  const authr = request.headers.get('authorization')
-  if (authr !== SECRET) throw error(401, 'Unauthorized')
+export const GET = async ({ url, fetch }) => {
+  const urlParam = url.searchParams.get('url')
+  if (!urlParam) throw error(400, 'url parameter is missing')
 
-  const u = url.searchParams.get('url')
-  if (!u) throw error(400, 'url parameter is missing')
-
-  const res = await fetch(u)
+  const res = await fetch(urlParam)
   if (!res.ok) throw error(502, `Failed to fetch url. Status code: ${res.status}`)
 
   const html = await res.text()
